5b0e84fbd9578740619ccc4976ef127219c33e0d
Oscar Montoya <oscardmg@gmail.com>
2019-04-07 22:44:42 -0500

    primer commit

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="jumbotron jumbtron-fluid">
+    <div class="display-4 text-center">
+      <img src="~/assets/img/food.png" alt="logo" >
+      Restauran App.</div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 offset-md-1">
+          <nuxt />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ pages/order/index.vue
@@ -0,0 +1,107 @@
+<template >
+  <div>
+    <form action="" autocomplete="off">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label for="OrderNo" >Order No.</label >
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <div class="input-group-text">#</div>
+                <input name="OrderNo" id="OrderNo" v-model="formData.orderNo" type="text" class="form-control" >
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="Customer" >Customer</label >
+            <input name="Customer" id="Customer" v-model="formData.customer" type="text" class="form-control" >
+          </div>
+          <div class="form-group">
+            <label for="PMethod" >Payment Method</label >
+            <select name="PMethod" id="PMethod" v-model="formData.paymentMethod" class="form-control">
+              <option value="" >-Select-</option >
+              <option value="Cash" >Cash</option >
+              <option value="Card" >Card</option >
+            </select >
+          </div>
+          <div class="form-group">
+            <label for="Total" >Grand Total</label >
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <div class="input-group-text">#</div>
+                <input name="Total" id="Total" v-model="formData.total" type="text" class="form-control" >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Order items table -->
+      <table class="table table-borderless">
+        <thead class="thead-light">
+        <th>Food</th>
+        <th>Price</th>
+        <th>Quantity</th>
+        <th>Total</th>
+        <th>
+          <a class="btn btn-sm btn-success text-white" @click="AddOrEditOrderItem(formData.OrderID)"><i class="fa fa-plus"></i> Add Item</a>
+        </th>
+        </thead>
+        <tbody>
+        <tr v-if="orderItems.length === 0">
+          <td class="font-italic text-center" colspan="5">
+            No food item selected for this order.
+          </td>
+        </tr>
+
+        <tr v-for="item in orderItems">
+          <td>{{item.ItemName}}</td>
+          <td>{{item.Price}}</td>
+          <td>{{item.Quantity}}</td>
+          <td>{{item.Total}}</td>
+          <td>
+            <a class="btn btn-sm btn-info text-white" @click="AddOrEditOrderItem(i,service.formData.OrderID)"><i class="fa fa-pencil"></i></a>
+            <a class="btn btn-sm btn-danger text-white ml-1" @click="onDeleteOrderItem(item.OrderItemID,i)"><i class="fa fa-trash"></i></a>
+          </td>
+        </tr>
+        </tbody>
+
+      </table>
+
+      <div class="form-group">
+        <button type="submit" class="btn btn-dark" >
+          <i class="fa fa-database"></i>
+          Submit
+        </button >
+      </div>
+    </form >
+  </div>
+</template >
+
+<script >
+  export default {
+    name: 'index',
+    data() {
+      return {
+        formData: {
+          orderNo: 0,
+          customer: '',
+          paymentMethod: '',
+          total: 0
+        },
+        orderItems: []
+      }
+    },
+    methods: {
+      AddOrEditOrderItem(){
+
+      },
+      onDeleteOrderItem() {
+
+      }
+    }
+  };
+</script >
+
+<style scoped >
+
+</style >
